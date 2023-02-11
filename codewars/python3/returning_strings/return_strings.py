7d427a8475b3fceea5481a76ca0ea4053a339fa9
Dominic <dsal.7k@gmail.com>
2023-02-11 16:38:54 +0800

    update solution for *.py

--- codewars/python3/returning_strings/return_strings.py
+++ codewars/python3/returning_strings/return_strings.py
@@ -1,7 +1,10 @@
 # Python3.10.6
 
 '''
+Returning Strings
 https://www.codewars.com/kata/55a70521798b14d4750000a4/train/python
+
+Instructions
 Make a function that will return a greeting statement that uses an
 input; your program should return, 
 
@@ -10,7 +13,7 @@ input; your program should return,
 [Make sure you type the exact thing I wrote or the program may not
 execute properly]
 
-Solution
+Solution template
 def greet(name):
     #Good Luck (like you need it)
     pass
--- intro_to_python/counting_sheep/count_sheeps.py
+++ intro_to_python/counting_sheep/count_sheeps.py
@@ -1,67 +1,32 @@
+# WSL2: Ubuntu
+# Python 3.10.6
+
 '''
 Counting sheep...
 https://www.codewars.com/kata/54edbc7200b811e956000556/python
 
-Consider an array/list of sheep where
-some sheep may be missing from their place.
-We need a function that counts the number
-of sheep present in the array (true means present).
+Instructions
+Consider an array/list of sheep where some sheep may be
+missing from their place. We need a function that counts
+the number of sheep present in the array (true means present).
 
 For example,
-=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=
+
 [True,  True,  True,  False,
   True,  True,  True,  True ,
   True,  False, True,  False,
   True,  False, False, True ,
   True,  True,  True,  True ,
   False, False, True,  True]
-=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=
-
 The correct answer would be 17.
+
 Hint: Don't forget to check for bad values like null/undefined
+
+Solution template
+def count_sheeps(sheep):
+  # TODO May the force be with you
+  pass
 '''
 
 def count_sheeps(sheep):
-    
-    '''
-    Naming variable tips:
-    What does the variable do?
-    - it counts sheeps:
-          - the variable is a "sheep_counter"
-    '''
-    
-    sheep_counter = 0 
-    for sheeps in sheep:
-        if sheeps:
-            sheep_counter += 1        
-        else:
-            # Don't increment counter
-            # when value is not eval to True
-            continue
-        
-    return sheep_counter
-
-# Main program
-sheeps = [True,  True,  True,  False,
-         True,  True,  True,  True,
-         True,  False, True,  False,
-         True,  False, False, True ,
-         True,  True,  True,  True ,
-         False, False, True,  True
-         ]
-
-number = count_sheeps(sheeps)
-print(number)
-
-'''
-Tips:
-rename variables to make autocomplete work faster
-
-DO:'p'rint('s'heep_counter)
-
-DON'T: 'p'rint('p'present_sheep)
-
-Interferes with intellisense autocompletion.
-Having very different variable names eases
-the use of autocompletion
-'''
+  return sheep.count(True)
