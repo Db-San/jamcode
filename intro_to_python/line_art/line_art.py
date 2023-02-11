34bcf3f044415d2deea3ee925689a6b7621893d8
Dominic <dsal.7k@gmail.com>
2023-02-12 01:16:51 +0800

    dump line art rewrite

--- intro_to_python/line_art/line_art.py
+++ intro_to_python/line_art/line_art.py
@@ -1,18 +1,10 @@
 # WSL2: Ubuntu
 # Python 3.10.6
 
-def display_line_art(number: int) -> int:
+# Scalable line art header
+def display_line_art(number: int) -> str:
+    ''' set number to set its length '''
     string = "=0"
     return (string*number+"=")    
 
-# Set length of line header
-length = 20
-
-print(display_line_art(length))
-print("AI: What is your favorite food?")
-# user_input = input("> ")
-user_input = "pizza"
-
-print(display_line_art(length))
-print(f"AI: {user_input.title()} is my favorite food too!")
-print(display_line_art(length))
+print(display_line_art(20))
