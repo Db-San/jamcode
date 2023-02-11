03ae4a9460cd52e71ab4b24258cb555595f8a181
Dominic <dsal.7k@gmail.com>
2023-02-11 17:19:36 +0800

    move test_yourself -> intro_to_pytnon

---
+++ intro_to_python/test_yourself/test_yourself.py
@@ -0,0 +1,49 @@
+import sys
+
+# WSL2: Ubuntu
+# Python 3.10.6
+
+'''
+Test Yourself
+https://www.joelonsoftware.com/2005/12/29/test-yourself/
+
+Content
+Never one to hold a grudge, I share those midterm questions with you…
+see if you can do better than I did freshman year.
+
+1a. (MIT-Scheme) Using the function
+Implement sum-of-squares, which calculates the 
+sum of squares of a list, for example:
+
+(sum-of-squares '(1 2 3 4 5))
+should evaluate to 55.
+'''
+def add_sum_of_squares(numbers):
+    return sum(int(number)**2 for number in numbers)
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+print(add_sum_of_squares(numbers))
+
+'''
+(ANSI C) Write a C program of the following form:
+
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+   ???
+}
+such that, after compiling it, it can be executed as
+
+% ./a.out could harold eat eight salami elephants
+'''
+
+def display_first_char_args():
+    arguments = list(sys.argv)
+    word = []
+    for argument in arguments:
+        word.append(list(argument))
+
+    return "".join([letter for index, letter in enumerate(word) if index])
+
+display_first_char_args()
