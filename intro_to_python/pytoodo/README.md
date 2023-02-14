3cedc8b43a2211f1b9f6274306759b8ec2a94276
Dominic <dsal.7k@gmail.com>
2023-02-15 03:40:25 +0800

    pytoodo dump

---
+++ intro_to_python/pytoodo/README.md
@@ -0,0 +1,49 @@
+# Pytoodo
+
+A Todo List CLI application written in Python.
+It allows users to manage their daily tasks and
+keep track of their progress in a terminal window.
+This terminal CLI app is inspired by the **Study Together** discord server.
+
+## Features
+
+- **Stay on track!** Add tasks as needed. Delete tasks when you're done.
+- Persistent data **in-between** sessions! The app can remember past sessions and can show past tasks.
+- It just works and **respects your privacy**.
+- **This CLI app** does not sshare data to 3rd-party companies. All user data stays on your device.
+- **Free and Open Source** As it should be.
+
+## Requirements
+
+- Python 3.x
+
+## Installation
+
+To run/install Pytoodo, follow these steps:
+
+1. Download the 
+Change into the directory of the cloned repository:
+shell
+Copy code
+$ cd todo-list-cli
+Run the following command to install the required packages:
+ruby
+Copy code
+$ pip install -r requirements.txt
+Usage
+To run the Todo List CLI application, use the following command:
+
+ruby
+Copy code
+$ python todo.py
+Features
+Add a task to the Todo List
+Mark a task as complete
+View all tasks
+Remove a task
+Quit the application
+Contributing
+Contributions are always welcome! If you have any suggestions or find any bugs, please open an issue on the GitHub repository.
+
+License
+This project is licensed under the MIT License.
---
+++ intro_to_python/pytoodo/pytoodo.py
@@ -0,0 +1,104 @@
+import os
+import platform
+import sys
+import datetime
+
+def clear_screen():
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def display_tasks():
+    if not os.path.exists("pytasks.txt") or os.stat("pytasks.txt").st_size == 0:
+        print("You have no tasks for today! Try adding some tasks below.")
+        return
+
+    with open("pytasks.txt", "r") as file:
+        print("Your current tasks:")
+        tasks = file.readlines()
+    for i, task in enumerate(tasks, 1):
+        print(f"{i}. {task.strip()}")
+
+
+def add_task():
+    clear_screen()
+    display_header()
+    display_tasks()
+    task = input("\nEnter your task:\n> ")
+    with open("pytasks.txt", "a") as file:
+        file.write(f"{task}\n")
+    print("Task added successfully!")
+
+def delete_task():
+    clear_screen()
+    display_header()
+    display_tasks()
+    task_index = False
+    while task_index is False:
+        try:
+            task_index = int(input("\nEnter the number of the task you want to delete: "))
+        except Exception:
+            clear_screen()
+            display_header()
+            display_tasks()
+            continue
+        
+    with open("pytasks.txt", "r") as file:
+        tasks = file.readlines()
+    tasks = [task for task in tasks if task.strip()]
+    if task_index < 1 or task_index > len(tasks):
+        print("Invalid task number. Try again.")
+        return
+    with open("pytasks.txt", "w") as file:
+        for i, task in enumerate(tasks, 1):
+            if i != task_index:
+                file.write(f"{task}")
+    print("Task deleted successfully!")
+
+def display_header():
+    ver = "0.1.1"
+    app_ver= f"[Version {ver}]"
+    app_title = "Pytoodo"
+    
+    length = 28
+    header = f"{app_title} | {app_ver}"
+    
+    print(""+"="*length)
+    print(header)
+    print(""+"="*length)
+    
+def display_choices():
+        print("\nChoices: ")
+        print("[1] - Show all tasks")
+        print("[2] - Add task")
+        print("[3] - Delete task")
+        print("[q] - Quit")
+
+def main():
+    display_header()
+    while True:
+        clear_screen()
+        display_header()    
+        display_tasks()
+        display_choices()
+        choice = input("Enter your choice [1-3, q(uit)]\n> ")
+        
+        if choice == "1":
+            clear_screen()
+            display_header()
+            display_tasks()
+
+        elif choice == "2":
+            add_task()
+        elif choice == "3":
+            delete_task()
+        elif choice.upper() == "Q":
+            clear_screen()
+            sys.exit()
+        else:
+            display_header()
+            print("Invalid choice. Try again.")
+
+if __name__ == "__main__":
+    main()
