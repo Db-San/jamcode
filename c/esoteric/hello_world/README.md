b404058a4e6a07391c2ff96f544790a585b8e6ca
db-san <dsal.7k@gmail.com>
2023-03-04 02:12:08 +0800

    Create README.md documentation init for c

---
+++ c/esoteric/hello_world/README.md
@@ -0,0 +1,5 @@
+# Special Credits
+
+Code created by Alice[@b1ek](https://github.com/b1ek)
+Compile for a quick [Sanity Check](https://en.wikipedia.org/wiki/Sanity_check)
+First seen on a mutual Discord Server
---
+++ c/esoteric/hello_world/hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+     char H = 'H';
+     char e = 'e';
+     char l = 'l';
+          l = 'l';
+     char o = 'o';
+     char _ = ' ';
+     char w = 'w';
+          o = 'o';
+     char r = 'r';
+          l = 'l';
+     char d = 'd';
+     char NL = '\n';
+
+     printf("%c%c%c%c%c%c%c%c%c%c%c%c", H, e, l, l, o, _, w, o, r, l, d, NL);
+}
